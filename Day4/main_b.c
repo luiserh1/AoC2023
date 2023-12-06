0ddeeaf935f3547b21e74ef258fd95ff1750f715
luis.serranohernandez.1415@gmail.com <$F*yfJSd7ZHWzLfyL>
2023-12-07 00:52:19 +0100

    Day4 a (WIP)

---
+++ Day4/main_b.c
@@ -0,0 +1,233 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+	void* next;
+
+	uint8_t num;
+} CardSeriesNumber;
+
+typedef struct
+{
+	void* previous;
+
+	CardSeriesNumber* ownNumsHead;
+	CardSeriesNumber* winNumsHead;
+
+	uint8_t index;
+	uint8_t amount;
+} Card;
+
+void freeCardSeriesNumber(CardSeriesNumber* set)
+{
+	CardSeriesNumber* current = set;
+	while (current != NULL)
+	{
+		CardSeriesNumber* aux = (CardSeriesNumber*)current->next;
+		free(current);
+		current = aux;
+	}
+}
+
+void freeCardFromTail(Card* game)
+{
+	Card* currentGame = game;
+	while (currentGame != NULL)
+	{
+		Card* aux = (Card*)currentGame->previous;
+		freeCardSeriesNumber(currentGame->ownNumsHead);
+		freeCardSeriesNumber(currentGame->winNumsHead);
+		free(currentGame);
+		currentGame = aux;
+	}
+}
+
+static inline FILE* readFile(const char* filename)
+{
+	// Open the file in read mode
+	FILE* fptr = fopen(filename, "r");
+	return fptr;
+}
+
+static inline int intValueOfChar(char digit)
+{
+	return (int)digit - 48;
+}
+
+int main(int argc, const char* argv[])
+{
+	//// Argument parsing and file reading
+	
+	// The program accepts only one argument:
+	// - the path of the file to read
+	if (argc != 2)
+	{
+		printf("Usage: %s <filename>\n", argv[0]);
+		return 1;
+	}
+
+	// Reading the example data file
+	const char* filename = argv[1];
+	FILE* fptr = readFile(filename);
+	if (fptr == NULL)
+	{
+		printf("Cannot open file %s\n", filename);
+		return 2;
+	}
+
+	//// Work
+
+	// Parsing file into data structure
+	Card* cardTail = NULL;
+
+	Card* nextCard = NULL;
+
+	char line[125];
+	const char delimSection1[2] = ":";
+	const char delimSection2[2] = "|";
+	const char delimBetweenNumbers[2] = " ";
+	while (fgets(line, sizeof(line), fptr) != NULL)
+	{
+		// Line format:
+		// Card <index>: <player_number>* | <winning_number>*
+		// Sections:
+		//  - First -       - Second -        - Third -
+		// Section Delimiters:
+		//             :                  |
+
+		// Section 1
+		char* section1Token = strtok(line, delimSection1);
+		char* restOfLine = strtok(NULL, delimSection1);
+		//printf("Section 1: %s\n", section1Token);
+		int cardIndex;
+		sscanf(section1Token, "Card %d", &cardIndex);
+		//printf("Card Index: %d\n", cardIndex);
+		Card* newCard = malloc(sizeof(Card));
+		if (newCard == NULL)
+			return 1;
+		newCard->index = cardIndex;
+		newCard->amount = 1;
+
+		// Section 2
+		CardSeriesNumber* parseNumsToken(char* numToken)
+		{
+			CardSeriesNumber* resHead = NULL;
+			while (numToken != NULL /*&& numToken != '\0'*/)
+			{
+				int num;
+				sscanf(numToken, "%d", &num);
+				CardSeriesNumber* newSeriesNum = malloc(sizeof(CardSeriesNumber));
+				if (newSeriesNum == NULL)
+					return NULL;
+				newSeriesNum->num = num;
+				newSeriesNum->next = resHead;
+				resHead = newSeriesNum;
+
+				numToken = strtok(NULL, delimBetweenNumbers);
+			}
+			return resHead;
+		}
+
+		char* section2Token = strtok(restOfLine, delimSection2);
+		restOfLine = strtok(NULL, delimSection2);
+		//printf("Section 2: %s\n", section2Token);
+		char* numToken = strtok(section2Token, delimBetweenNumbers);
+		newCard->ownNumsHead = parseNumsToken(numToken);
+		if (newCard->ownNumsHead == NULL)
+			return 2;
+
+		// Section 3
+		//printf("Section 3: %s\n", restOfLine);
+		numToken = strtok(restOfLine, delimBetweenNumbers);
+		newCard->winNumsHead = parseNumsToken(numToken);
+		if (newCard->winNumsHead == NULL)
+			return 3;
+
+		// Line fully processed
+		if (nextCard == NULL)
+		{
+			nextCard->previous = newCard;
+		}
+		else
+		{
+			cardTail = newCard;
+		}
+
+		nextCard = newCard;
+	}
+	if (cardTail->previous == NULL)
+		printf("Fuck!\n");
+
+	// Computing result
+
+	int sumOfCards = 0;
+	Card* currentCard = cardTail;
+	while (currentCard != NULL)
+	{
+		printf("Checking Card %d...\n", currentCard->index);
+		int winningPointsAmount = 0;
+
+		CardSeriesNumber* currentOwnNumber = currentCard->ownNumsHead;
+		while (currentOwnNumber != NULL)
+		{
+			CardSeriesNumber* previousWinningNumber = NULL;
+			CardSeriesNumber* currentWinningNumber = currentCard->winNumsHead;
+			while (currentWinningNumber != NULL)
+			{
+				if (currentOwnNumber->num == currentWinningNumber->num)
+				{
+					printf("\t + %d is a winning number\n", currentOwnNumber->num);
+					winningPointsAmount++;
+
+					CardSeriesNumber* auxNext = currentWinningNumber->next;
+					// Removing from winning numbers list
+					if (previousWinningNumber == NULL)
+						currentCard->winNumsHead = currentWinningNumber->next;
+					else
+						previousWinningNumber->next = currentWinningNumber->next;
+					free(currentWinningNumber);
+					currentWinningNumber = auxNext;
+				}
+				else
+				{
+					previousWinningNumber = currentWinningNumber;
+					currentWinningNumber = currentWinningNumber->next;
+				}
+			}
+
+			currentOwnNumber = currentOwnNumber->next;
+		}
+
+		Card* previousCard = currentCard->previous;
+		for (int i = winningPointsAmount; winningPointsAmount > 0; winningPointsAmount--)
+		{
+			if (previousCard == NULL)
+				break;
+
+			previousCard->amount += currentCard->amount;
+
+			previousCard = previousCard->previous;
+		}
+
+		sumOfCards += currentCard->amount;
+		printf("\t > Amount of scratchcards: %d\n", currentCard->amount);
+
+		currentCard = currentCard->previous;
+	}
+
+	// Result
+
+	printf("The result is: %d\n", sumOfCards);
+
+	//// Cleanup
+
+	// Free allocated memory
+	freeCardFromTail(cardTail);
+
+	//// Succes
+
+	return 0;
+}

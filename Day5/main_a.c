6c0619d664cb54bd5b83e6b49dc107e6d77979a6
luis.serranohernandez.1415@gmail.com <$F*yfJSd7ZHWzLfyL>
2023-12-10 20:29:27 +0100

    Day5 a (WIP)

--- Day5/main_a.c
+++ Day5/main_a.c
@@ -10,13 +10,27 @@ typedef struct
 	uint32_t num;
 } SeedIdNode;
 
+// The values from sourceRangeStart onward are mapped to destinationRangeStart onward
+// This rule applies for rangeLength values
+typedef struct
+{
+	void* previous;
+
+	uint32_t destinationRangeStart;
+	uint32_t sourceRangeStart;
+	uint32_t rangeLength;
+} MapWithRanges;
+
 ///////////////////////////////////////////////////////////////////////////////
 
 static inline FILE* readFile(const char*);
 void freeSeedNodes(SeedIdNode*);
 void printSeeds(SeedIdNode*);
-void printMap(uint32_t*, uint32_t, char*, char*);
 static inline int isDigit(char);
+int insertOrdered(MapWithRanges**, uint32_t, uint32_t, uint32_t);
+uint32_t getMappedValue(MapWithRanges*, uint32_t);
+void printMapWithRanges(MapWithRanges*, uint32_t, char*, char*);
+uint32_t getPreviousInCyclicArray(uint32_t*, long, long, long);
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -25,6 +39,17 @@ static inline FILE* readFile(const char* filename)
 	// Open the file in read mode
 	FILE* fptr = fopen(filename, "r");
 	return fptr;
+}
+
+uint32_t getPreviousInCyclicArray(uint32_t* arr, long length, long currentIndex, long prev)
+{
+	long prevIndex;
+	if (prev > currentIndex)
+		prevIndex = length - (prev - currentIndex);
+	else
+		prevIndex = currentIndex - prev;
+
+	return arr[prevIndex];
 }
 
 void freeSeedNodes(SeedIdNode* seed)
@@ -50,31 +75,101 @@ void printSeeds(SeedIdNode* seed)
 	printf("\n");
 }
 
-void printMap(uint32_t* map, uint32_t length, char* from, char* to)
+void printMapWithRanges(MapWithRanges* map, uint32_t maxValue, char* from, char* to)
 {
+	static const long NUM_OF_SAME_DIFFS_UNTIL_HIDE = 2;
+	static const long NUM_OF_LAST_NUMS_DISPLAYED = 2;
+
+	uint32_t prevNumbers[NUM_OF_SAME_DIFFS_UNTIL_HIDE];
+	memset(prevNumbers, 0, sizeof(uint32_t) * NUM_OF_SAME_DIFFS_UNTIL_HIDE);
+	long prevNumbersI = 0;
+
 	printf("%-15s%-15s\n", from, to);
 	uint32_t loopsSinceChange = 0;
 	int lastDif = 0;
-	for (uint32_t i = 0; i < length; i++)
+	for (uint32_t i = 0; i < maxValue; i++)
 	{
-		if (i - map[i] == lastDif)
+		uint32_t value = getMappedValue(map, i);
+		if (i - value == lastDif)
 		{
 			loopsSinceChange++;
 		}
 		else
 		{
-			lastDif = i - map[i];
-			if (loopsSinceChange > 3)
-				i -= 2;
+			lastDif = i - value;
+			if (loopsSinceChange > NUM_OF_SAME_DIFFS_UNTIL_HIDE)
+			{
+				for (long j = 0; j < NUM_OF_SAME_DIFFS_UNTIL_HIDE; j++)
+				{
+					long prevIndex  = i - NUM_OF_SAME_DIFFS_UNTIL_HIDE + j;
+					long prevAmount = NUM_OF_SAME_DIFFS_UNTIL_HIDE - j;
+					long prevNum    = getPreviousInCyclicArray(prevNumbers, NUM_OF_SAME_DIFFS_UNTIL_HIDE, prevNumbersI, prevAmount);
+					printf("%-15ld%-15ld\n", prevIndex, prevNum);
+				} 
+			}
 			loopsSinceChange = 0;
 		}
 
-		if (loopsSinceChange < 3 || i > length - 3)
-			printf("%-15d%-15d\n", i, map[i]);
+		if (loopsSinceChange <= NUM_OF_SAME_DIFFS_UNTIL_HIDE || i > maxValue - NUM_OF_LAST_NUMS_DISPLAYED)
+			printf("%-15d%-15d\n", i, value);
 
-		if (loopsSinceChange == 3)
+		if (loopsSinceChange == NUM_OF_SAME_DIFFS_UNTIL_HIDE + 1)
 			printf("%-15s%-15s\n", "...", "...");
+
+		prevNumbers[prevNumbersI] = value;
+		prevNumbersI++;
+		if (prevNumbersI == NUM_OF_SAME_DIFFS_UNTIL_HIDE)
+			prevNumbersI = 0;
 	}
+}
+
+int insertOrdered(MapWithRanges** map, uint32_t destinationRangeStart, uint32_t sourceRangeStart, uint32_t rangeLength)
+{
+	MapWithRanges* newMap = malloc(sizeof(map));
+	if (newMap == NULL)
+		return 0;
+
+	newMap->destinationRangeStart = destinationRangeStart;
+	newMap->sourceRangeStart      = sourceRangeStart;
+	newMap->rangeLength           = rangeLength;
+	newMap->previous              = NULL;
+
+	if (*map == NULL)
+	{
+		*map = newMap;
+		return 1;
+	}
+
+	MapWithRanges* current = *map;
+	while (current != NULL)
+	{
+		if (sourceRangeStart < current->sourceRangeStart)
+		{
+			newMap->previous  = current->previous;
+			current->previous = newMap;
+			break;
+		}
+		current = (*map)->previous;
+	}
+
+	return 1;
+}
+
+uint32_t getMappedValue(MapWithRanges* map, uint32_t at)
+{
+	MapWithRanges* current = map;
+	while (current != NULL)
+	{
+		if (at >= current->sourceRangeStart && at < current->sourceRangeStart + current->rangeLength)
+		{
+			uint32_t posInRange = at - current->sourceRangeStart;
+			return current->destinationRangeStart + posInRange;
+		}
+
+		current = current->previous;
+	}
+
+	return at;
 }
 
 static inline int isDigit(char c)
@@ -107,14 +202,14 @@ int main(int argc, const char* argv[])
 	}
 
 	//// Work
-	SeedIdNode* seedsHead;                // seeds ids
-	uint32_t* seedToSoilMap;              // key: uint32_t index (seed id),        value: uint32_t value in memory (soil id)
-	uint32_t* soilToFertilizerMap;        // key: uint32_t index (soil id),        value: uint32_t value in memory (fertilizer id)
-	uint32_t* fertilizerToWaterMap;;      // key: uint32_t index (fertilizer id),  value: uint32_t value in memory (water id)
-	uint32_t* waterToLightMap;;           // key: uint32_t index (water id),       value: uint32_t value in memory (light id)
-	uint32_t* lightToTemperatureMap;;     // key: uint32_t index (light id),       value: uint32_t value in memory (temperature id)
-	uint32_t* temperatureToHumidityMap;;  // key: uint32_t index (temperature id), value: uint32_t value in memory (humidity id)
-	uint32_t* humidityToLocationMap;;     // key: uint32_t index (humidity id),    value: uint32_t value in memory (location id)
+	SeedIdNode* seedsHead                   = NULL;
+	MapWithRanges* seedToSoilMap            = NULL;
+	MapWithRanges* soilToFertilizerMap      = NULL;
+	MapWithRanges* fertilizerToWaterMap     = NULL;
+	MapWithRanges* waterToLightMap          = NULL;
+	MapWithRanges* lightToTemperatureMap    = NULL;
+	MapWithRanges* temperatureToHumidityMap = NULL;
+	MapWithRanges* humidityToLocationMap    = NULL;
 
 	uint32_t maxSeedId = 0; // This value will be used to calculate the requiered of the maps
 
@@ -122,7 +217,7 @@ int main(int argc, const char* argv[])
 	char line[250];
 	if (fgets(line, sizeof(line), fptr) == NULL)
 		return 1;
-	char* token = strtok(line, " "); // "seeds:"
+	/*char* token = */strtok(line, " "); // "seeds:"
 	char* seedToken = strtok(NULL, " "); // the rest of the line (ints separated by white spaces)
 	while (seedToken != NULL)
 	{
@@ -149,28 +244,17 @@ int main(int argc, const char* argv[])
 
 	// parsing: maps
 	
-	if (fgets(line, sizeof(line), fptr) == NULL) // expected blank line before seed-to-soil map
+	if (fgets(line, sizeof(line), fptr) == NULL) // expected blank line before "seed-to-soil" map
 		return 2;
-	
-	uint32_t* allocateMap()
-	{
-		uint32_t* map = malloc(sizeof(uint32_t) * mapsSize);
-		if (map == NULL)
-			return NULL;
-		for (uint32_t i = 0; i < mapsSize; i++)
-			map[i] = i;
 
-		return map;
-	}
-
-	int processNextMap(uint32_t* map)
+	int processNextMap(MapWithRanges** map)
 	{
 		if (fgets(line, sizeof(line), fptr) == NULL) // expected "name-of-map map:" line
 			return 0;
 
 		while (fgets(line, sizeof(line), fptr) != NULL)
 		{
-			if (!isDigit(line[0])) // expected blank line before soil-to-fertilizer
+			if (!isDigit(line[0])) // expected: blank line before next map || the end of the file
 				break;
 
 			char* destinetionRangeStartToken = strtok(line, " ");
@@ -181,8 +265,8 @@ int main(int argc, const char* argv[])
 			uint32_t sourceRangeStart      = atoi(sourceRangeStartToken);
 			uint32_t rangeLength           = atoi(rangeLengthToken);
 
-			for (uint32_t i = 0; i < rangeLength; i++)
-				map[sourceRangeStart + i] = destinetionRangeStart + i;
+			if (!insertOrdered(map, destinetionRangeStart, sourceRangeStart, rangeLength))
+				return 0;
 		}
 
 		return 1;
@@ -190,52 +274,45 @@ int main(int argc, const char* argv[])
 
 	// seed-to-soil
 	printf("___________________________\n");
-	seedToSoilMap = allocateMap();
-	if (!processNextMap(seedToSoilMap))
+	if (!processNextMap(&seedToSoilMap))
 		return 3;
-	printMap(seedToSoilMap, mapsSize, "Seed", "Soil");
+	printMapWithRanges(seedToSoilMap, mapsSize, "Seed", "Soil");
 
 	// soil-to-fertilizer
 	printf("___________________________\n");
-	soilToFertilizerMap = allocateMap();
-	if (!processNextMap(soilToFertilizerMap))
+	if (!processNextMap(&soilToFertilizerMap))
 		return 4;
-	printMap(soilToFertilizerMap, mapsSize, "Soil", "Fertilizer");
+	printMapWithRanges(soilToFertilizerMap, mapsSize, "Soil", "Fertilizer");
 
 	// fertilizer-to-water
 	printf("___________________________\n");
-	fertilizerToWaterMap = allocateMap();
-	if (!processNextMap(fertilizerToWaterMap))
-		return 4;
-	printMap(fertilizerToWaterMap, mapsSize, "Fertilizer", "Water");
+	if (!processNextMap(&fertilizerToWaterMap))
+		return 5;
+	printMapWithRanges(fertilizerToWaterMap, mapsSize, "Fertilizer", "Water");
 
 	// water-to-light
 	printf("___________________________\n");
-	waterToLightMap = allocateMap();
-	if (!processNextMap(waterToLightMap))
-		return 4;
-	printMap(waterToLightMap, mapsSize, "Water", "Light");
+	if (!processNextMap(&waterToLightMap))
+		return 6;
+	printMapWithRanges(waterToLightMap, mapsSize, "Water", "Light");
 
 	// light-to-temperature
 	printf("___________________________\n");
-	lightToTemperatureMap = allocateMap();
-	if (!processNextMap(lightToTemperatureMap))
-		return 4;
-	printMap(lightToTemperatureMap, mapsSize, "Light", "Temperature");
+	if (!processNextMap(&lightToTemperatureMap))
+		return 7;
+	printMapWithRanges(lightToTemperatureMap, mapsSize, "Light", "Temperature");
 
 	// temperature-to-humidity
 	printf("___________________________\n");
-	temperatureToHumidityMap = allocateMap();
-	if (!processNextMap(temperatureToHumidityMap))
-		return 4;
-	printMap(temperatureToHumidityMap, mapsSize, "Temperature", "Humidity");
+	if (!processNextMap(&temperatureToHumidityMap))
+		return 8;
+	printMapWithRanges(temperatureToHumidityMap, mapsSize, "Temperature", "Humidity");
 
 	// humidity-to-location
 	printf("___________________________\n");
-	humidityToLocationMap = allocateMap();
-	if (!processNextMap(humidityToLocationMap))
-		return 4;
-	printMap(humidityToLocationMap, mapsSize, "Humidity", "Location");
+	if (!processNextMap(&humidityToLocationMap))
+		return 9;
+	printMapWithRanges(humidityToLocationMap, mapsSize, "Humidity", "Location");
 
 	// computing
 	int minLocation = INT_MAX;
@@ -245,19 +322,19 @@ int main(int argc, const char* argv[])
 	{
 		uint32_t seed = currentNode->num;
 		printf("Seed % d -> ", seed);
-		uint32_t soil = seedToSoilMap[seed];
+		uint32_t soil = getMappedValue(seedToSoilMap, seed);
 		printf("Soil %d -> ", soil);
-		uint32_t fert = soilToFertilizerMap[soil];
+		uint32_t fert = getMappedValue(soilToFertilizerMap, soil);
 		printf("Fertilizer %d -> ", fert);
-		uint32_t water = fertilizerToWaterMap[fert];
+		uint32_t water = getMappedValue(fertilizerToWaterMap, fert);
 		printf("Water %d -> ", water);
-		uint32_t light = waterToLightMap[water];
+		uint32_t light = getMappedValue(waterToLightMap, water);
 		printf("Light %d -> ", light);
-		uint32_t temp = lightToTemperatureMap[light];
+		uint32_t temp = getMappedValue(lightToTemperatureMap, light);
 		printf("Temperature %d -> ", temp);
-		uint32_t humi = temperatureToHumidityMap[temp];
+		uint32_t humi = getMappedValue(temperatureToHumidityMap, temp);
 		printf("Humidity %d -> ", humi);
-		uint32_t loc = humidityToLocationMap[humi];
+		uint32_t loc = getMappedValue(humidityToLocationMap, humi);
 		printf("[Locations %d]\n", loc);
 
 		if (loc < minLocation)
